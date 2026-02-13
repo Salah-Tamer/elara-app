cdf6c6d0a6cb401fc5e7774e52191c60e585c1bd
maryamabdallah <maryamabdallah698@gmail.com>
2026-02-13 11:42:03 +0200

    Initial Flutter project with MVVM + Cubit architecture

---
+++ android/app/src/main/kotlin/com/example/elara/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.elara
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Elara());
+}
+
+class Elara extends StatelessWidget {
+  const Elara({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
